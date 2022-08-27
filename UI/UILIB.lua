05d13838cf4ccab5b6a34932cac9a9810e094813
Mark <94270731+HexerMaster1929@users.noreply.github.com>
2022-08-27 01:14:15 -0400

    Update UILIB.lua

--- UI/UILIB.lua
+++ UI/UILIB.lua
@@ -120,11 +120,11 @@ local themeStyles = {
 		ElementColor = Color3.fromRGB(170, 170, 255)
 	},
 	HexHub = {
-		SchemeColor = Color3.fromRGB(0, 166, 58),
-		Background = Color3.fromRGB(31, 41, 43),
-		Header = Color3.fromRGB(22, 29, 31),
-		TextColor = Color3.fromRGB(255,255,255),
-		ElementColor = Color3.fromRGB(22, 29, 31)
+		SchemeColor = Color3.fromRGB(170, 170, 255),
+		Background = Color3.fromRGB(255, 255, 255),
+		Header = Color3.fromRGB(170, 170, 255),
+		TextColor = Color3.fromRGB(255, 255, 255),
+		ElementColor = Color3.fromRGB(255, 255, 255)
 	}
 }
 local oldTheme = ""
