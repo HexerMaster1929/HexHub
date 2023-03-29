7515818fef4358d66a5cd6acdaac98c3146987be
Mark <94270731+HexerMaster1929@users.noreply.github.com>
2023-03-28 21:28:23 -0400

    Update V2.lua

--- Extras/DHANTISTAR/V2.lua
+++ Extras/DHANTISTAR/V2.lua
@@ -4,9 +4,12 @@ local List = UI.Main.StarList
 local SettingsFrame = UI.Main.Settings
 local LPLR = game.Players.LocalPlayer
 local Assets = UI.Assets
-	local Request = http_request or syn and syn.request or request or nil
+local Request = http_request or syn and syn.request or request or (fluxus and fluxus.request) or nil
+assert((Request),"Cannot Start Antistar, Exploit Requires Request Function")
 local MessageBox = loadstring(game:HttpGet("https://raw.githubusercontent.com/HexerMaster1929/HexHub/main/Extras/DHANTISTAR/HexMes_Messagebox.lua"))()
 local MessageName;
+
+Ver = "2.0B"
 
 local RunService = game:GetService("RunService")
 local CoreGui = game:GetService("CoreGui")
@@ -19,7 +22,7 @@ local Themes = {
 
 
 
-local SHOPMODULE = loadstring(game:HttpGet"https://raw.githubusercontent.com/HexerMaster1929/HexHub/main/Extras/DHANTISTAR/ServerHop.lua")()
+--local SHOPMODULE = loadstring(game:HttpGet"https://raw.githubusercontent.com/HexerMaster1929/HexHub/main/Extras/DHANTISTAR/ServerHop.lua")()
 
 Settings = {
 	SearchInterval = 3, -- interval between automatically searching for stars in server (in seconds)
@@ -32,30 +35,52 @@ Star = "⭐" or "U+2B50"
 StarTable = {}
 local WarningBlacklists = {}
 
-local HTTP = game:GetService("HttpService")
+HTTP = game:GetService("HttpService")
+Players = game:GetService("Players")
+UserInputService = game:GetService("UserInputService")
+TweenService = game:GetService("TweenService")
+MarketplaceService = game:GetService("MarketplaceService")
+RunService = game:GetService("RunService")
+TeleportService = game:GetService("TeleportService")
+StarterGui = game:GetService("StarterGui")
+GuiService = game:GetService("GuiService")
+Lighting = game:GetService("Lighting")
+ContextActionService = game:GetService("ContextActionService")
+NetworkClient = game:GetService("NetworkClient")
+ReplicatedStorage = game:GetService("ReplicatedStorage")
+GroupService = game:GetService("GroupService")
+PathService = game:GetService("PathfindingService")
+SoundService = game:GetService("SoundService")
+Teams = game:GetService("Teams")
+StarterPlayer = game:GetService("StarterPlayer")
+InsertService = game:GetService("InsertService")
+ChatService = game:GetService("Chat")
+ProximityPromptService = game:GetService("ProximityPromptService")
+StatsService = game:GetService("Stats")
+MaterialService = game:GetService("MaterialService")
 
 function GetInfo(Plr)
 	--local InfoAPI = "https://users.roproxy.com/v1/users/"..tostring(Plr.UserId)
 	local Data;
 	local Success,Fail = pcall(function()
-	local r = Request(
-		{
-			['Method'] = 'GET',
-			['Headers'] = {
-			--	['discordLinkRequest'] = "true"
-			},
+		local r = Request(
+			{
+				['Method'] = 'GET',
+				['Headers'] = {
+					--	['discordLinkRequest'] = "true"
+				},
 				['Url'] = 'https://users.roblox.com/v1/users/'..tostring(Plr.UserId)
-		})
+			})
 		if r.StatusCode ~= 200 or not r.Successful then
 			warn("Success")
-		Data = r.Body
-		--	warn(Data)
+			Data = r.Body
+			--	warn(Data)
 		else
-		
-		Data = r.Body
-		--	warn(Data)
+
+			Data = r.Body
+			--	warn(Data)
 		end
-		end)
+	end)
 	if Success and Data then
 		warn(HTTP:JSONDecode(Data))
 		return HTTP:JSONDecode(Data)
@@ -186,7 +211,60 @@ List.Inner.Buttons.SetCancel.Cancel.MouseButton1Down:Connect(function()
 	List.Visible = false
 end)
 List.Inner.Buttons.SetCancel.SHOP.MouseButton1Down:Connect(function()
-	SHOPMODULE:Teleport(game.PlaceId)
+	--SHOPMODULE:Teleport(game.PlaceId)
+	local PlaceId, JobId = game.PlaceId, game.JobId
+	if Request then
+		local servers = {}
+		local req = Request({Url = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100", PlaceId)})
+		local body = HTTP:JSONDecode(req.Body)
+		if body and body.data then
+			for i, v in next, body.data do
+				if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= JobId then
+					table.insert(servers, 1, v.id)
+				end
+			end
+		end
+		if #servers > 0 then
+			TeleportService:TeleportToPlaceInstance(PlaceId, servers[math.random(1, #servers)], Players.LocalPlayer)
+		else
+
+			local MName = tostring(math.random(1,100000000))
+			MessageBox.Show({
+				MessageName = MName, -- required if you add a custom close button to the message
+				Title = "Error!",
+				Content = "Couldn't Find A Server!",
+				BorderColor3 = Themes[Settings.CurrentTheme].BorderColor,
+				TargetFrame = UI.Main.CornerHolder.Notifications,
+				Length = 5.6,
+				Buttons = {
+					{
+						Text = "Close",
+						Callback = function() 
+							MessageBox.Close(UI.Main.CornerHolder.Notifications,MName) -- MsgName Goes In here too
+						end,
+					},
+				}
+			})
+		end
+	else
+		local MName = tostring(math.random(1,100000000))
+		MessageBox.Show({
+			MessageName = MName, -- required if you add a custom close button to the message
+			Title = "Error!",
+			Content = "Your Exploit Does Not Support HTTP Requests",
+			BorderColor3 = Themes[Settings.CurrentTheme].BorderColor,
+			TargetFrame = UI.Main.CornerHolder.Notifications,
+			Length = 5.6,
+			Buttons = {
+				{
+					Text = "Close",
+					Callback = function() 
+						MessageBox.Close(UI.Main.CornerHolder.Notifications,MName) -- MsgName Goes In here too
+					end,
+				},
+			}
+		})
+	end
 end)
 List.Inner.Buttons.SetCancel.Leave.MouseButton1Down:Connect(function()
 	game.Shutdown()
@@ -242,7 +320,7 @@ MessageName = tostring(math.random(1,100000000))
 MessageBox.Show({
 	MessageName = MessageName, -- required if you add a custom close button to the message
 	Title = "Success!",
-	Content = "Antistar V2.0A - Loaded Successfully!",
+	Content = "Antistar V"..Ver.." - Loaded Successfully!",
 	BorderColor3 = Themes[Settings.CurrentTheme].BorderColor,
 	TargetFrame = UI.Main.CornerHolder.Notifications,
 	Length = 5.6,
